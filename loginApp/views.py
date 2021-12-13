11b0e808a15ef802d422a9a48afc04aa96e6992d
Courdevelops <courdevelops@gmail.com>
2021-12-13 15:35:39 -0500

    loginlogic logout button added

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 #HOME
 def home(request):
@@ -13,8 +14,10 @@ def logged(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, ('Login Success'))
             return redirect('logged')
         else:
+            messages.success(request, ('Error Logging In'))
             return redirect('home')
     else:
         return render(request, "logged.html", {}) 
