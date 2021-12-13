e78450a41c7691ea38f9ecc77396eb0b31b2c587
Courdevelops <courdevelops@gmail.com>
2021-12-13 15:23:08 -0500

    loginlogic basic login

--- loginApp/views.py
+++ loginApp/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 
+#HOME
 def home(request):
     return render(request, "home.html", {}) 
 
+#LOGIN
 def logged(request):
     if request.method == "POST":
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('logged')
+        else:
+            return redirect('home')
     else:
         return render(request, "logged.html", {}) 
 
