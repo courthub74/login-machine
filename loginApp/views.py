004b30d05e91d02418858dcad71c338253da3ef3
Courdevelops <courdevelops@gmail.com>
2021-12-15 17:51:14 -0500

    Salesforce login function is stuck at home page

--- loginApp/views.py
+++ loginApp/views.py
@@ -31,18 +31,17 @@ def logout_user(request):
 #SALESFORCE DEMO PAGE
 def sfdemo(request):
     return render(request, "sfdemo.html", {})
-    
 
 #SALESFORCE LOGIN
-def salesforce_login(request):
+def sflogin(request):
     if request.method == "POST":
-        sfname = request.POST['sfname']
-        sfpassword = request.POST['sfpassword']
-        user = authenticate(request, sfname=sfname, sfpassword=sfpassword)
+        sfuser = request.POST['sfuser']
+        sfpass = request.POST['sfpass']
+        user = authenticate(request, sfuser=sfuser, sfpass=sfpass)
         if user is not None:
             login(request, user)
             return redirect('sfdemo')
         else:
             return redirect('home')
-    else: #If nothing happens just go back to home page
-        return render(request, "home.html", {})
+    else:
+        return render(request, "home.html", {}) 
