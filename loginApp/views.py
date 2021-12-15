341f40718ee3853fb172974ab1496b818c7b4b70
Courdevelops <courdevelops@gmail.com>
2021-12-15 18:11:17 -0500

    Salesforce login function redirect needs investigating

--- loginApp/views.py
+++ loginApp/views.py
@@ -35,13 +35,15 @@ def sfdemo(request):
 #SALESFORCE LOGIN
 def sflogin(request):
     if request.method == "POST":
-        sfuser = request.POST['sfuser']
-        sfpass = request.POST['sfpass']
-        user = authenticate(request, sfuser=sfuser, sfpass=sfpass)
+        username2 = request.POST['username2'] #username2 is what is entered in the input area of name='username2'
+        password2 = request.POST['password2'] #password2 is what is entered in the input area of name='password2'
+        user = authenticate(request, username=username2, password=password2)
         if user is not None:
             login(request, user)
+            messages.success(request, ('Login Success'))
             return redirect('sfdemo')
         else:
+            messages.success(request, ('Error Logging In'))
             return redirect('home')
     else:
         return render(request, "home.html", {}) 
